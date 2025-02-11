d1414df1345e6577686199f8297933aa9dc9ee26
Tarfprima <granta.150@gmail.com>
2025-02-11 20:49:19 +0300

    Добавил измениния по требованию классной работы

---
+++ 20250211_JS_DOM/work2.js
@@ -0,0 +1,12 @@
+
+function addEventListener(array){   
+    console.log('Страница загрузилась');
+}  
+
+function word() {}
+    console.log(addEventListener);
+
+
+function is_even(n){}
+console.log(console.log('И как же её запустить???'))
+
