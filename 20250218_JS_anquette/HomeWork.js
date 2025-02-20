27a45d007d2b52e9fe0856e3cd275287c9106961
Tarfprima <granta.150@gmail.com>
2025-02-20 17:57:24 +0300

    gen

---
+++ 20250218_JS_anquette/HomeWork.js
@@ -0,0 +1,8 @@
+const gen = () => {
+    const firstName = document.querySelector('#firstName').value;
+    const lastName = document.querySelector('#lastName').value;
+
+
+
+    console.log(` ${firstName} ${lastName}, поздравляем Вас с Днем рождения!`)
+};
